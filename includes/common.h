d26e9ef98a6e57d9570fbd2682fb1b28ad75644e
AlvaroPV <100451185@alumnos.uc3m.es>
2022-10-06 21:00:34 +0200

    Ceated parser to check all the input data (may not work)

--- includes/common.h
+++ includes/common.h
@@ -2,7 +2,14 @@
 // Created by xxmid on 02/10/2022.
 //
 
+
 #ifndef ARCOS_PROJECT1_COMMON_H
 #define ARCOS_PROJECT1_COMMON_H
+
+#include <iostream>
+#include <string>
+#include <filesystem>
+
+int arg_parser (int argc, char *argv[]);
 
 #endif //ARCOS_PROJECT1_COMMON_H
