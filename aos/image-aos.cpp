b9bfd1b4b22d0c2863cd2015c2f88693df033041
MiDDiz <xxmidone30xx@gmail.com>
2022-10-20 15:49:33 +0200

    fixed bug where we are setting color out of bounds

--- aos/image-aos.cpp
+++ aos/image-aos.cpp
@@ -3,13 +3,20 @@
 //
 
 #include "../includes/common.h"
+#include "../includes/aos.h"
 
-void trabaja(std::filesystem::path filePath){
+void trabaja(const std::filesystem::path& filePath, char *pathOutDir, void (*func)(BmpAOS)){
     BmpAOS bmp;
 
     std::string prefix = "new_";
-    bmp.Read(filePath.string().c_str());
-    bmp.Export( prefix.append(filePath.string()).c_str());
+    bmp.Read(filePath);
+
+    std::filesystem::path out_path = std::filesystem::path(pathOutDir);
+    std::cout << out_path / filePath.filename() << std::endl;
+
+    func(bmp);
+
+    bmp.Export(out_path / filePath.filename());
 }
 
 int functionality(std::vector<std::filesystem::path>BmpPaths, std::string lastarg,std::filesystem::path endpath ){
@@ -18,6 +25,8 @@ int functionality(std::vector<std::filesystem::path>BmpPaths, std::string lastar
         if (lastarg=="copy"){
             FileCopy(BMP, endpath);
         }
+        if (lastarg=="gauss");
+            trabaja(path1, path2, gaus)
     }
     return (0);
 }
