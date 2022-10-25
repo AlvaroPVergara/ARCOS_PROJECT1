7530de85f918e66889b8bf2b1e5e1d0340b90164
AlvaroPV <100451185@alumnos.uc3m.es>
2022-10-25 20:02:44 +0200

    Refactor and commented Imagesoa

--- soa/imagesoa.cpp
+++ soa/imagesoa.cpp
@@ -7,38 +7,48 @@
 #include "../includes/HistoSOA.h"
 #include "MonoSOA.cpp"
 
-void execute_function(const std::filesystem::path& filePath, std::filesystem::path pathOutDir, int(*function)(BmpSOA&)){
+
+/*ExecuteFunction is used for execute the mono and gauss functions, it takes the path of the file (BMP)
+ * and the exit directory for the path, it also takes what function it needs to be done
+ * (MonoSOA/gaussianTransformation).*/
+void execute_function(const std::filesystem::path& filePath, std::filesystem::path path_out_dir, int(*function)(BmpSOA&)){
+    //First, it creates the BmpSOA objet and fills it with the path given
     BmpSOA bmp;
     std::string prefix = "new_";
     bmp.Read(filePath);
 
-    std::filesystem::path out_path = std::filesystem::path(pathOutDir);
-
+    //Then it executes the third parameter function
     function(bmp);
 
-    if (bmp.Export(out_path / filePath.filename()) < 0)
+    //When the function finishes it exports the Bmp file to the output directory
+    if (bmp.Export(path_out_dir / filePath.filename()) < 0)
     {
         exit(-1);
     }
 }
 
+/*ExecuteHisto is the adapted version of ExecuteFunction to work with histograms.*/
 void ExecuteHisto(const std::filesystem::path& file_path, std::filesystem::path path_out_dir){
+    //Besides creating a Bmp object, it also creates an HistoAos objet
     BmpSOA bmp;
     HistoSOA hs;
 
     std::string prefix = "new_";
     bmp.Read(file_path);
 
-    std::filesystem::path out_path = std::filesystem::path(path_out_dir);
+    //For teh output file name, it will be the output directory plus the name of the file without
+    //extension and then the changed extension to .txt
     std::filesystem::path new_filename = file_path.filename().replace_extension(".txt");
-    std::string new_path = out_path.generic_string()+"/HistoSOA-"+ new_filename.generic_string();
+    std::string new_path = path_out_dir.generic_string()+"/HistoSOA-"+ new_filename.generic_string();
 
+    //Lastly it creates the histogram
     if (hs.histogram(bmp,new_path)<0){
         std::cerr<< "Failed creating histo" << std::endl;
     }
 }
 
-
+/*Functionality takes a vector of BMP paths, then, for each path, it calls the execution function
+ * that the user chooses in the input arguments*/
 int Functionality(std::vector<std::filesystem::path>BmpPaths, std::string lastarg, std::filesystem::path endpath ){
     for (const auto &path :BmpPaths)
     {
