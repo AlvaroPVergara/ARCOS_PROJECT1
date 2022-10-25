38641f1adb9c58250e522a3a76a5e9eb4c3417f6
AlvaroPV <100451185@alumnos.uc3m.es>
2022-10-25 19:28:51 +0200

    Copy commented

--- common/copy.cpp
+++ common/copy.cpp
@@ -4,9 +4,14 @@
 
 #include "../includes/common.h"
 
+/*The function Filecopy gets 2 paths from param, first one is the file from the source path
+ * from the file that needs to be copied, the second one is the directory where it is going to be copied.
+ * It returns 0 on success and -1 when fails */
 int FileCopy(const std::filesystem::path& source, const std::filesystem::path& target)
 {
+    //First, the path of the copy is going to be the path of the target directory plus the name of the file
     std::filesystem::path finalpath = target / source.filename();
+    //Then it copies the file and returns -1 if fails
     if (!std::filesystem::copy_file(source, finalpath, std::filesystem::copy_options::overwrite_existing))
     {
         std::cerr << "Fallo al copiar archivo"<<std::endl;
